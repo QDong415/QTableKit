6fe2ec23b9926e878f78c88431c84468a8dfbf13
285275534 <285275534@qq.com>
2022-08-05 21:37:51 +0800

    Initial commit

---
+++ Example/QTableKit/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  QTableKit
+//
+//  Created by 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 on 08/05/2022.
+//  Copyright (c) 2022 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. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
